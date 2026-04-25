811b5e0ae794bc0e91f753f7f854f5eafd2aef6c
Abhay <abhay@Abhays-MacBook-Pro.local>
2026-04-25 22:37:48 +0530

    feat: add theme state and routing for Home and Signpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
+import React, { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Home from "./Pages/Home"
 import Signpage from "./Pages/Signpage"
 
 function App() {
+  const [theme, setTheme] = useState('dark')
 
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home theme={theme} setTheme={setTheme} />} />
         <Route path="/login" element={<Signpage />} />
       </Routes>
     </Router>
